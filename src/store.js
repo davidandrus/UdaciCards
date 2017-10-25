838e10095e3a6d0078fa4dff51e9611a5963a85d
davidandrus <davidandrus.com@gmail.com>
2017-10-25 16:24:34 -0700

    adding redux-logger

--- src/store.js
+++ src/store.js
@@ -1,4 +1,8 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import { createLogger } from "redux-logger";
+
+const logger = createLogger();
+
 import reducer from "./reducers/root";
 
-export default createStore(reducer);
+export default createStore(reducer, applyMiddleware(logger));
