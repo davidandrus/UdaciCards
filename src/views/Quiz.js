851c5d9b4ae69e329f8a3678911bec5a436891e1
davidandrus <davidandrus.com@gmail.com>
2017-10-31 10:04:30 -0700

    updating quiz to show a hardcoded question

--- src/views/Quiz.js
+++ src/views/Quiz.js
@@ -1,6 +1,30 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { connect } from "react-redux";
 
-const Quiz = () => <Text>Quiz Will Go Here</Text>;
+import Button from "../components/Button";
 
-export default Quiz;
+const Quiz = ({ name, question, answer }) => (
+  <View>
+    <Text>Quiz for: {name}</Text>
+    <Text>1/1</Text>
+    <Text>{question}</Text>
+    <Button text="Show Answer" onPress={() => {}} />
+    <Button text="Correct" onPress={() => {}} />
+    <Button text="Incorrect" onPress={() => {}} />
+  </View>
+);
+
+const mapStateToProps = (state, { navigation }) => {
+  const deckId = navigation.state.params.id;
+  const deck = state.decks.find(deck => deck.id === deckId);
+  const { question, answer } = deck.cards[0];
+
+  return {
+    ...deck,
+    question,
+    answer
+  };
+};
+
+export default connect(mapStateToProps)(Quiz);
